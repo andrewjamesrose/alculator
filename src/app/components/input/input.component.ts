0755350c3b577f4e6de4b66998a0052252a565ec
unknown <andrewjamesrose@gmail.com>
2022-09-13 08:26:49 +0800

    added a number of modules; placeholder services; limited forms

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -8,6 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class InputComponent implements OnInit {
 
   constructor() { }
+
+  
+    bibationForm = new FormGroup({
+        date: new FormControl(''),
+        drink: new FormControl<string>('')
+    })
+
+
+    drinkForm = new FormGroup({
+        name: new FormControl<string|null>(''),
+        abv: new FormControl<number|null>(null),
+        volume: new FormControl<number|null>(null),
+    })
+
 
   ngOnInit(): void {
   }
