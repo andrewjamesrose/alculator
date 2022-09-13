0209c4dce6d0488bcfecff3b0a28563eeffd9904
unknown <andrewjamesrose@gmail.com>
2022-09-13 23:09:30 +0800

    added basic graph/table/features

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { DRINK_NAMED_VOLUMES } from 'src/app/common/constants';
 
 @Component({
   selector: 'app-input',
@@ -8,12 +9,20 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class InputComponent implements OnInit {
 
-  constructor() { }
+    quantities: number[]
+
+    constructor() {
+        this.quantities =  Array(12).fill(0).map((x,i)=>i+1)
+    }
+
+    namedVolumes = DRINK_NAMED_VOLUMES
 
   
     bibationForm = new FormGroup({
-        date: new FormControl(''),
-        drink: new FormControl<string>('')
+        date: new FormControl<Date>(new Date()),
+        drink: new FormControl<string>(''),
+        namedVol: new FormControl<string>(''),
+        quantity: new FormControl<number>(1)
     })
 
 
@@ -22,6 +31,10 @@ export class InputComponent implements OnInit {
         abv: new FormControl<number|null>(null),
         volume: new FormControl<number|null>(null),
     })
+
+    // preDefinedVol = new FormGroup({
+
+    // })
 
 
   ngOnInit(): void {
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -9,7 +9,68 @@ export class TableComponent implements OnInit {
 
   constructor() { }
 
+  columnFields: string[] = ['date', 'volume', 'abv', 'units_per', 'quantity', 'units_total', 'edit', 'delete']
+
+  
+  demoStatsTable = [
+    {
+        id:         123,
+        date:       '06-Aug-22',
+        volume:     440,
+        abv:        5,
+        quantity:   2
+    },
+    {
+        id:         234,
+        date:       '07-Aug-22',
+        volume:     250,
+        abv:        12,
+        quantity:   1
+    },
+    {
+        id:         345,
+        date:       '08-Aug-22',
+        volume:     568,
+        abv:        4.2,
+        quantity:   3
+    },
+    {
+        id:         456,
+        date:       '09-Aug-22',
+        volume:     50,
+        abv:        40,
+        quantity:   2
+    },
+  ]
+
   ngOnInit(): void {
   }
 
+    calculateUnits(row: ResultRow): number {
+        return row.abv * row.volume / 1000
+    }
+
+
+    calculateTotalUnits(row: ResultRow): number {
+        return row.quantity * this.calculateUnits(row)
+    }
+
+
+    handleRowDelete(id: number):void {
+        console.log("Deleting row: " + id)
+    }
+
+    handleRowEdit(id: number):void {
+        console.log("Editing row: " + id)
+    }
+
+}
+
+
+interface ResultRow {
+    id:         number,
+    date:       string
+    volume:     number,
+    abv:        number,
+    quantity:   number
 }
